306acf987704dfceaacbf3d16def2990e8e941ae
[bmlindor] <blindor@uw.edu>
2021-09-22 15:14:51 -0700

    updated julia dir

--- Julia/compute_ttv.jl
+++ Julia/compute_ttv.jl
@@ -2,33 +2,24 @@
 # in eccentricity.  Please cite Agol & Deck (2015) if
 # you make use of this in published research.
 
-module TTVFaster
+# module TTVFaster
 
-VERSION < v"0.4-dev" && using Docile
+# # VERSION < v"0.4-dev" && using Docile
 
-export Planet_plane_hk, compute_ttv!
-export Planet_plane                   # Deprecated, only exported to make the error message work
+# export Planet_plane_hk, compute_ttv!
+# export Planet_plane                   # Deprecated, only exported to make the error message work
 
-#include("ttv_coeff.jl")    # TODO: Any reason not to delete this?
 include("ttv_succinct.jl")
 
-include("laplace_coefficients_initialize.jl")
-laplace_coefficients_initialize(jmax::Integer,alpha::Number) = LaplaceCoefficients.initialize(jmax,alpha)
-export laplace_coefficients_initialize
-
-immutable Planet_plane
-# Parameters of a planet in a plane-parallel system
-  # Mass ratio of the planet to the star:
+struct Planet_plane
   mass_ratio :: Float64
-  # Initial time of transit:
   period   :: Float64
   trans0   :: Float64
-  eccen    :: Float64
-# longitude of periastron measured from line of sight, in radians:
-  omega    :: Float64
+  ecc    :: Float64  
+  omega    :: Float64   
 end
 
-type Planet_plane_hk{T<:Number} # Parameters of a planet in a plane-parallel system
+struct Planet_plane_hk{T<:Real} # Parameters of a planet in a plane-parallel system
   # Mass ratio of the planet to the star:
   mass_ratio :: T
   # Initial time of transit:
@@ -39,120 +30,124 @@ type Planet_plane_hk{T<:Number} # Parameters of a planet in a plane-parallel sys
   esinw    :: T
 end
 
-"""
-# Error message to explain to anyone who tries to use the old version
-"""
-function compute_ttv!(jmax::Integer,p1::Planet_plane,p2::Planet_plane,time1::Vector,time2::Vector,ttv1::Vector,ttv2::Vector,f1::Array,f2::Array,b::Array,alpha0::Number,b0::Array) 
+
+# """
+# # Error message to explain to anyone who tries to use the old version
+# """
+function compute_ttv!(jmax::Integer,p1::Planet_plane,p2::Planet_plane,time1::Vector,time2::Vector,ttv1::Vector,ttv2::Vector)
   error("The Planet_plane data structure has been deprecated in favor of Planet_plane_hk")
 end
 
-"""
-# Computes transit-timing variations to linear order in
-# eccentricity for non-resonant, plane-parallel planets.
-# Input:
-#   jmax:  Maximum j over which to sum the TTV calculation for both planets
-#     p1:  Planet type for inner planet
-#     p2:  Planet type for outer planet
-#  time1:  Transit times for inner planet
-#  time2:  Transit times for outer planet
-# alpha0:  Initial alpha for Taylor expansion of coefficients
-#     b0:  Laplace coefficients and derivatives for use in Taylor expansion
-# Output:
-#   ttv1: TTVs of the inner planet
-#   ttv2: TTVs of the outer planet
-#     f1: TTV coefficients for inner planet
-#     f2: TTV coefficients for outer planet
-#      b: Laplace coefficients (& derivatives) for outer planet
-"""
-function compute_ttv!(jmax::Integer,p1::Planet_plane_hk,p2::Planet_plane_hk,time1::Vector,time2::Vector,ttv1::Vector,ttv2::Vector,f1::Array,f2::Array,b::Array,alpha0::Number,b0::Array)
+# """
+# # Computes transit-timing variations to linear order in
+# # eccentricity for non-resonant, plane-parallel planets.
+# # Input:
+# #   jmax:  Maximum j over which to sum the TTV calculation for both planets
+# #     p1:  Planet type for inner planet
+# #     p2:  Planet type for outer planet
+# #  time1:  Transit times for inner planet
+# #  time2:  Transit times for outer planet
+# #
+# # Output:
+# #   ttv1: TTVs of the inner planet
+# #   ttv2: TTVs of the outer planet
+# """
 
-# Compute the semi-major axis ratio of the planets:
-# println(p1.period,p2.period)
-const alpha = (p1.period/p2.period)^(2//3)  # Julia supports rational numbers!
-# Number of times:
-const ntime1 = length(time1)
-const ntime2 = length(time2)
-# Compute the coefficients:
-ttv_succinct!(jmax+1,alpha,f1,f2,b,alpha0,b0)  # I need to compute coefficients one higher than jmax
-# Compute TTVs for inner planet (equation 33):
-# Compute since of \pomegas:
-e1 = sqrt(p1.esinw*p1.esinw+p1.ecosw*p1.ecosw)
-e2 = sqrt(p2.esinw*p2.esinw+p2.ecosw*p2.ecosw)
-sin1om=p1.esinw/e1 
-sin2om=p2.esinw/e2
-cos1om=p1.ecosw/e1 
-cos2om=p2.ecosw/e2
-# Compute mean motions:
-n1=2pi/p1.period
-n2=2pi/p2.period
-# Compute initial longitudes:
-lam10=-n1*p1.trans0 + 2*p1.esinw # 2*p1.eccen*sin1om
-lam20=-n2*p2.trans0 + 2*p2.esinw # 2*p2.eccen*sin2om
-@inbounds for i=1:ntime1
-# Compute the longitudes of the planets at times of transit of planet 1 (equation 49):
-  lam11 = n1*time1[i]+lam10
-  lam21 = n2*time1[i]+lam20
-  psi1  = lam11-lam21 # Compute difference in longitudes at times of transit of planet 1
-  sinpsi1=sin(psi1)
-  cospsi1=cos(psi1)
-  sinlam11 = sin(lam11)
-  coslam11 = cos(lam11) 
-  sinlam1om1=sinlam11*cos1om-coslam11*sin1om
-  coslam1om1=coslam11*cos1om+sinlam11*sin1om
-  sinlam1om2=sinlam11*cos2om-coslam11*sin2om
-  coslam1om2=coslam11*cos2om+sinlam11*sin2om
-  ttv1[i]=zero(p1.period) #0.0
-  sinjm1psi1=zero(p1.period) #0.0
-  cosjm1psi1=one(p1.period) #1.0
-# Sum over j:
-  for j=1:jmax
-    sinjpsi1=sinjm1psi1*cospsi1+cosjm1psi1*sinpsi1
-    cosjpsi1=cosjm1psi1*cospsi1-sinjm1psi1*sinpsi1
-    ttv1[i] += f1[j+1,1]*sinjpsi1
-    ttv1[i] += f1[j+1,2]*e1*(sinjpsi1*coslam1om1-cosjpsi1*sinlam1om1)
-    ttv1[i] += f1[j+1,3]*e1*(sinjpsi1*coslam1om1+cosjpsi1*sinlam1om1)
-    ttv1[i] += f1[j  ,4]*e2*(sinjpsi1*coslam1om2-cosjpsi1*sinlam1om2)
-    ttv1[i] += f1[j+2,5]*e2*(sinjpsi1*coslam1om2+cosjpsi1*sinlam1om2)
-    sinjm1psi1=sinjpsi1
-    cosjm1psi1=cosjpsi1
+function compute_ttv!(jmax::Integer,p1::Planet_plane_hk{T},p2::Planet_plane_hk{T},time1::Vector{T},time2::Vector{T},ttv1::Vector{T},ttv2::Vector{T}) where T<:Real
+
+  # Compute the semi-major axis ratio of the planets:
+  # println(p1.period,p2.period)
+  global alpha = (p1.period/p2.period)^(2//3)  #check memory allocation >>>>>>>>>>>>
+  #println(alpha, p1.period, p2.period)
+  @assert(alpha < 1)
+  @assert(alpha > 0)
+  # Number of times:
+   ntime1 = length(time1)
+   ntime2 = length(time2)
+  f1=zeros(T,jmax+2,5)  #check memory allocation >>>>>>>>>>>>
+  f2=zeros(T,jmax+2,5)  #check memory allocation >>>>>>>>>>>>
+  # Compute the coefficients:
+  ttv_succinct!(jmax+1,alpha,f1,f2)  # I need to compute coefficients one higher than jmax
+  # Compute TTVs for inner planet (equation 33):
+  # Compute since of \pomegas:
+  e1 = sqrt(p1.esinw*p1.esinw + p1.ecosw*p1.ecosw)
+  e2 = sqrt(p2.esinw*p2.esinw + p2.ecosw*p2.ecosw)
+  sin1om=p1.esinw/e1
+  sin2om=p2.esinw/e2
+  cos1om=p1.ecosw/e1
+  cos2om=p2.ecosw/e2
+
+  # Compute mean motions:
+  n1=2pi/p1.period
+  n2=2pi/p2.period
+  # Compute initial longitudes:
+  lam10=-n1*p1.trans0 + 2*p1.esinw # 2*p1.eccen*sin1om
+  lam20=-n2*p2.trans0 + 2*p2.esinw # 2*p2.eccen*sin2om
+  @inbounds for i=1:ntime1
+  # Compute the longitudes of the planets at times of transit of planet 1 (equation 49):
+    lam11 = n1*time1[i]+lam10
+    lam21 = n2*time1[i]+lam20
+    psi1  = lam11-lam21 # Compute difference in longitudes at times of transit of planet 1
+    sinpsi1=sin(psi1)
+    cospsi1=cos(psi1)
+    sinlam11 = sin(lam11)
+    coslam11 = cos(lam11)
+    sinlam1om1=sinlam11*cos1om - coslam11*sin1om
+    coslam1om1=coslam11*cos1om + sinlam11*sin1om
+    sinlam1om2=sinlam11*cos2om - coslam11*sin2om
+    coslam1om2=coslam11*cos2om + sinlam11*sin2om
+    ttv1[i]=zero(T) #0.0
+    sinjm1psi1=zero(T) #0.0
+    cosjm1psi1=one(T) #1.0
+  # Sum over j:
+    for j=1:jmax
+      sinjpsi1=sinjm1psi1*cospsi1 + cosjm1psi1*sinpsi1
+      cosjpsi1=cosjm1psi1*cospsi1 - sinjm1psi1*sinpsi1
+      ttv1[i] += f1[j+1,1]*sinjpsi1
+      ttv1[i] += f1[j+1,2]*e1*(sinjpsi1*coslam1om1 - cosjpsi1*sinlam1om1)
+      ttv1[i] += f1[j+1,3]*e1*(sinjpsi1*coslam1om1 + cosjpsi1*sinlam1om1)
+      ttv1[i] += f1[j  ,4]*e2*(sinjpsi1*coslam1om2 - cosjpsi1*sinlam1om2)
+      ttv1[i] += f1[j+2,5]*e2*(sinjpsi1*coslam1om2 + cosjpsi1*sinlam1om2)
+      sinjm1psi1=sinjpsi1
+      cosjm1psi1=cosjpsi1
+    end
+  # Multiply by period and mass ratio, and divide by 2*Pi:
+    ttv1[i] = ttv1[i]*p1.period*p2.mass_ratio/(2pi)
   end
-# Multiply by period and mass ratio, and divide by 2*Pi:
-  ttv1[i] = ttv1[i]*p1.period*p2.mass_ratio/(2pi)
-end
-# Compute TTVs for outer planet (equation 33):
-@inbounds for i=1:ntime2
-# Compute the longitudes of the planets at times of transit of planet 2:
-  lam12 = n1*time2[i]+lam10
-  lam22 = n2*time2[i]+lam20
-  sinlam22 = sin(lam22)
-  coslam22 = cos(lam22) 
-  psi2  = lam12-lam22 # Compute difference in longitudes at times of transit of planet 2
-  sinpsi2=sin(psi2)
-  cospsi2=cos(psi2)
-  sinlam2om1=sinlam22*cos1om-coslam22*sin1om
-  coslam2om1=coslam22*cos1om+sinlam22*sin1om
-  sinlam2om2=sinlam22*cos2om-coslam22*sin2om
-  coslam2om2=coslam22*cos2om+sinlam22*sin2om
-  ttv2[i]=zero(p2.period) #0.0
-  sinjm1psi2=zero(p2.period) #0.0
-  cosjm1psi2=one(p2.period) #1.0
-# Sum over j:
-  for j=1:jmax
-    sinjpsi2=sinjm1psi2*cospsi2+cosjm1psi2*sinpsi2
-    cosjpsi2=cosjm1psi2*cospsi2-sinjm1psi2*sinpsi2
-    ttv2[i] += f2[j+1,1]*sinjpsi2
-    ttv2[i] += f2[j+1,2]*e2*(sinjpsi2*coslam2om2-cosjpsi2*sinlam2om2)
-    ttv2[i] += f2[j+1,3]*e2*(sinjpsi2*coslam2om2+cosjpsi2*sinlam2om2)
-    ttv2[i] += f2[j+2,4]*e1*(sinjpsi2*coslam2om1-cosjpsi2*sinlam2om1)
-    ttv2[i] += f2[j  ,5]*e1*(sinjpsi2*coslam2om1+cosjpsi2*sinlam2om1)
-    sinjm1psi2=sinjpsi2
-    cosjm1psi2=cosjpsi2
+  # Compute TTVs for outer planet (equation 33):
+  @inbounds for i=1:ntime2
+  # Compute the longitudes of the planets at times of transit of planet 2:
+    lam12 = n1*time2[i]+lam10
+    lam22 = n2*time2[i]+lam20
+    sinlam22 = sin(lam22)
+    coslam22 = cos(lam22)
+    psi2  = lam12-lam22 # Compute difference in longitudes at times of transit of planet 2
+    sinpsi2=sin(psi2)
+    cospsi2=cos(psi2)
+    sinlam2om1=sinlam22*cos1om - coslam22*sin1om
+    coslam2om1=coslam22*cos1om + sinlam22*sin1om
+    sinlam2om2=sinlam22*cos2om - coslam22*sin2om
+    coslam2om2=coslam22*cos2om + sinlam22*sin2om
+    ttv2[i]=zero(p2.period) #0.0
+    sinjm1psi2=zero(p2.period) #0.0
+    cosjm1psi2=one(p2.period) #1.0
+  # Sum over j:
+    for j=1:jmax
+      sinjpsi2=sinjm1psi2*cospsi2 + cosjm1psi2*sinpsi2
+      cosjpsi2=cosjm1psi2*cospsi2 - sinjm1psi2*sinpsi2
+      ttv2[i] += f2[j+1,1]*sinjpsi2
+      ttv2[i] += f2[j+1,2]*e2*(sinjpsi2*coslam2om2 - cosjpsi2*sinlam2om2)
+      ttv2[i] += f2[j+1,3]*e2*(sinjpsi2*coslam2om2 + cosjpsi2*sinlam2om2)
+      ttv2[i] += f2[j+2,4]*e1*(sinjpsi2*coslam2om1 - cosjpsi2*sinlam2om1)
+      ttv2[i] += f2[j  ,5]*e1*(sinjpsi2*coslam2om1 + cosjpsi2*sinlam2om1)
+      sinjm1psi2=sinjpsi2
+      cosjm1psi2=cosjpsi2
+    end
+  # Multiply by period and mass ratio, and divide by 2*Pi:
+    ttv2[i] = ttv2[i]*p2.period*p1.mass_ratio/(2pi)
   end
-# Multiply by period and mass ratio, and divide by 2*Pi:
-  ttv2[i] = ttv2[i]*p2.period*p1.mass_ratio/(2pi)
-end
-# Finished!
-return
+  # Finished! 
+  #ttv1,ttv2 are already allocated
+  return 
 end  # compute_ttv!
-
-end # module
+# end # module
--- Julia/laplace_wisdom.jl
+++ Julia/laplace_wisdom.jl
@@ -9,70 +9,70 @@
 #   by series summation */
 #/* Code due to Jack Wisdom */
 """
-function laplace_wisdom(s::Rational,i::Integer,j::Integer,a::Number)
+function laplace_wisdom(s::Rational,i::Integer,j::Integer,a::T) where T<:Real
 # function laplace_wisdom,s,i,j,a  IDL
 # double laplace(double s, int i, int j, double a);  c
 
 ##define LAPLACE_EPS 1.0e-12
-const LAPLACE_EPS = convert(eltype(a),1.0e-12)
+  LAPLACE_EPS = convert(T,1.0e-12)
 
-#if (i lt 0) then i = -i
-i=abs(i)
+  #if (i lt 0) then i = -i
+  i=abs(i)
 
-if j <= i     #/* compute first term in sum */
-  factor4 = one(a)
-  for k=0:j-1
-    factor4 *= i - k
+  if j <= i     #/* compute first term in sum */
+    factor4 = one(T)
+    for k=0:j-1
+      factor4 *= i - k
+    end
+    lap_coef_sum = factor4
+    q0 = 0
+  else
+    q0 = fld(j + 1 - i,2) #largest integer less than or equal to x/y
+    lap_coef_sum = zero(T)
+    factor4 = one(T)
   end
-  lap_coef_sum = factor4
-  q0 = 0
-else
-  q0 = fld(j + 1 - i,2)
-  lap_coef_sum = zero(a)
-  factor4 = one(a)
-end
 
-#  /* compute factors for terms in lap_coef_sum */
+  #  /* compute factors for terms in lap_coef_sum */
 
-factor1 = s
-factor2 = s + i
-factor3 = i + 1
-for q=1:q0-1       #/* no contribution for q = 0 */
-  factor1 *= s + q
-  factor2 *= s + i + q
-  factor3 *= i+q+1
-end
-if q0 > 1
-  q=q0
-else
-  q=1
-end
-#println(j+1-i,q0)
-term = a*a * factor1 * factor2 / (factor3 * q)
-
-#  /* sum series */
-
-while  (term*factor4) > LAPLACE_EPS
-  factor4 = one(a)
-  for k=0:j-1
-    factor4 *= (2*q + i - k)
+  factor1 = s
+  factor2 = s + i
+  factor3 = i + 1
+  for q=1:q0-1       #/* no contribution for q = 0 */
+    factor1 *= s + q
+    factor2 *= s + i + q
+    factor3 *= i + q + 1
   end
-  lap_coef_sum += term * factor4
-  factor1 += 1
-  factor2 += 1
-  factor3 += 1
-  q = q+1
-  term *= a*a * factor1 * factor2 / (factor3 * q)
-end
+  if q0 > 1
+    q=q0
+  else
+    q=1
+  end
+  #println(j+1-i,q0)
+  term = a*a * factor1 * factor2 / (factor3 * q)
 
-#  /* fix coefficient */
+  #  /* sum series */
 
-for k=0:i-1
-  lap_coef_sum *= (s+k)/(k+1)
-end
+  while  (term*factor4) > LAPLACE_EPS
+    factor4 = one(T)
+    for k=0:j-1
+      factor4 *= (2*q + i - k)
+    end
+    lap_coef_sum += term * factor4
+    factor1 += 1
+    factor2 += 1
+    factor3 += 1
+    q = q+1
+    term *= a*a * factor1 * factor2 / (factor3 * q)
+  end
 
-apower = (q0 <= 0) ?  i : 2*q0 + i - 2
-lap_coef_sum *= 2 * a^apower
-# Return the Laplace Coefficient:
-return lap_coef_sum
+  #  /* fix coefficient */
+
+  for k=0:i-1
+    lap_coef_sum *= (s+k)/(k+1)
+  end
+
+  apower = (q0 <= 0) ?  i : 2*q0 + i - 2
+  lap_coef_sum *= 2 * a^apower
+  # Return the Laplace Coefficient:
+  return lap_coef_sum
 end
